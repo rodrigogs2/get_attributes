5bf5009cbe06b4cde0c372eeff2869ab3fe46554
Rodrigo Gomes - Desktop <rodrigogs2@gmail.com>
2020-02-10 20:34:06 -0300

    construcao de funcao que busca atributos da ref_slice para um dado volume informando-se a img_id

--- build_db_axis0_refimg.py
+++ build_db_axis0_refimg.py
@@ -7,6 +7,7 @@ Created on Fri Jan 31 18:00:54 2020
 """
 import os, csv, sys, re
 import numpy as np
+import list_dir
 
 __USE_PIT_IMAGES = True
 __STEP_TO_PIT = 2
@@ -69,66 +70,66 @@ def get_image_demographic_data_by_id2(img_id, demographics_dictionary):
     return subject_class_gender_sex_refslices
 
 
-def get_ref_slice_attributes(attributes_file, csv_dic, target_bplanes=[0]):
-    attributes_as_str = []
+#def get_ref_slice_attributes(attributes_file, csv_dic, target_bplanes=[0]):
+#    attributes_as_str = []
+#    
+#    img_id_str = get_image_ID2(attributes_file)
+#    
+#    try:
+#        img_id = int(img_id_str)
+#    except ValueError:
+#        print('This value ({0}) can not be converted to integer'.format(img_id_str))
+#    
+#    #csv_dic = build_csv_dictionary(csv_file)
+#    ref_slices = csv_dic['ref_slices']
+#    
+#    for tbp in target_bplanes: # for each target body plane, do it....
+#        if os.path.exists(attributes_file):
+#            try:
+#                with open(attributes_file, 'r') as file:
+#                    reader = csv.reader(file)
+#                    for row in reader:
+#                        bplane = row[0]
+#                        slicenum = row[1]
+#                        
+#                        try:
+#                            tsn = ref_slices[tbp] # target slice_num will be the ref_slice from this bplane
+#                        except ValueError:
+#                            print('*** Invalid target bplane ({0}). rec_slices={1}'.format(tbp,ref_slices))
+#                            
+#                        
+#                        if bplane == tbp and slicenum == tsn:
+#                            attributes_as_str = row[2:]
+#                        
+#            except os.error:
+#                print("*** ERROR: Attributes file %s can not be readed (os.error in load_attribs function)" % attributes_file)
+#            except UnicodeDecodeError:
+#                print('Error processing file ({0})'.format(attributes_file))    
+#        
+#    return attributes_as_str
     
-    img_id_str = get_image_ID2(attributes_file)
     
-    try:
-        img_id = int(img_id_str)
-    except ValueError:
-        print('This value ({0}) can not be converted to integer'.format(img_id_str))
-    
-    #csv_dic = build_csv_dictionary(csv_file)
-    ref_slices = csv_dic['ref_slices']
-    
-    for tbp in target_bplanes: # for each target body plane, do it....
-        if os.path.exists(attributes_file):
-            try:
-                with open(attributes_file, 'r') as file:
-                    reader = csv.reader(file)
-                    for row in reader:
-                        bplane = row[0]
-                        slicenum = row[1]
-                        
-                        try:
-                            tsn = ref_slices[tbp] # target slice_num will be the ref_slice from this bplane
-                        except ValueError:
-                            print('*** Invalid target bplane ({0}). rec_slices={1}'.format(tbp,ref_slices))
-                            
-                        
-                        if bplane == tbp and slicenum == tsn:
-                            attributes_as_str = row[2:]
-                        
-            except os.error:
-                print("*** ERROR: Attributes file %s can not be readed (os.error in load_attribs function)" % attributes_file)
-            except UnicodeDecodeError:
-                print('Error processing file ({0})'.format(attributes_file))    
-        
-    return attributes_as_str
-    
-    
-    attribs_as_floats = []
-
-    for str_attrib in attributes_as_str:
-        #print('String attribute: ', str_attrib)
-        if str_attrib != '':
-            try:
-                value = float(str_attrib)
-            except ValueError:
-                print('*** ERROR: Fail to convert an string attribute ("{0}") to float in load_attribs_and_metadata().\n Attributes File: {1}'.format(str_attrib, attributes_file))
-                sys.exit(-1)
-            
-            attribs_as_floats.append(value)
+#    attribs_as_floats = []
+#
+#    for str_attrib in attributes_as_str:
+#        #print('String attribute: ', str_attrib)
+#        if str_attrib != '':
+#            try:
+#                value = float(str_attrib)
+#            except ValueError:
+#                print('*** ERROR: Fail to convert an string attribute ("{0}") to float in load_attribs_and_metadata().\n Attributes File: {1}'.format(str_attrib, attributes_file))
+#                sys.exit(-1)
+#            
+#            attribs_as_floats.append(value)
                     
         
     # NumPy transformations
-    attribs = np.array(attribs_as_floats_lists, dtype=np.float64)
-    body_plane = np.array(body_plane_list, dtype=np.int64)
-    slice_numbers = np.array(slicenum_list, dtype=np.int64)
-    slice_amount = np.array(slice_amount_per_plane, dtype=np.int64)
-    
-    return  attribs, body_plane, slice_numbers, slice_amount
+#    attribs = np.array(attribs_as_floats_lists, dtype=np.float64)
+#    body_plane = np.array(body_plane_list, dtype=np.int64)
+#    slice_numbers = np.array(slicenum_list, dtype=np.int64)
+#    slice_amount = np.array(slice_amount_per_plane, dtype=np.int64)
+#    
+#    return  attribs, body_plane, slice_numbers, slice_amount
 
 def get_normalized_ref_slices_dic():
     return 0
@@ -172,7 +173,198 @@ def load_IDs_and_ref_img_from_axis(csv_file, axis_num=0):
         except UnicodeDecodeError:
             print('Error processing file ({0})'.format(csv_file))
     
+    return image_id_list, slicenum_list
+    
 
+def load_ref_slices_attribs(attributes_file, ref_slices):
+    #body_plane_list = []
+    #slicenum_list = []
+    ref_slices_attributes = []
+        
+    if os.path.exists(attributes_file):
+        for target_bp in range(len(ref_slices)):
+            target_refslice = ref_slices[target_bp]
+
+            
+            if target_refslice:
+                #print('target_bp={0} and target_refslice={1}'.format(target_bp, target_refslice))
+                try:
+                    with open(attributes_file, 'r') as file:
+                        reader = csv.reader(file)
+                        for row in reader:
+                            bplane = int(row[0])
+                            slicenum = int(row[1])
+                            #print('bplane={0}, slicenum={1}, target_bp={2}, target_refslice={3}'.format(bplane,slicenum,target_bp,target_refslice))
+                            
+                            if bplane == int(target_bp) and slicenum == int(target_refslice):
+                                attributes = row[2:]
+                                ref_slices_attributes.append(attributes)
+                                #print('ref slice attributes found: ',attributes)
+
+
+                except os.error:
+                    print("*** ERROR: Attributes file %s can not be readed (os.error in load_attribs function)" % attributes_file)
+                except UnicodeDecodeError:
+                    print('Error processing file ({0})'.format(attributes_file))
+            
+                attribs_as_floats_lists = []
+                #print("*** Processing attributes file %s)" % attributes_file)
+                for attribs_as_string in ref_slices_attributes:
+                    #print("Attribs as strings: ", attribs_as_string)
+                    a = []
+                    for str_attrib in attribs_as_string:
+                        #print('String attribute: ', str_attrib)
+                        if str_attrib != '':
+                            try:
+                                value = float(str_attrib)
+                            except ValueError:
+                                print('*** ERROR: Fail to convert an string attribute ("{0}") to float in load_attribs_and_metadata().\n Attributes File: {1}'.format(str_attrib, attributes_file))
+                                sys.exit(-1)
+                            
+                            a.append(value)
+                                
+                    attribs_as_floats_lists.append(a)
+        
+        # Counting slices from each body axis
+    #    plane0 = body_plane_list.count('0') - 1 #256 slices are indexed between 0 and 255
+    #    plane1 = body_plane_list.count('1') - 1
+    #    plane2 = body_plane_list.count('2') - 1
+        
+        #plane0 = len(body_plane_list) - body_plane_list[::-1].index('0')
+        #plane1 = len(body_plane_list) - body_plane_list[::-1].index('1') - plane0
+        #plane2 = len(body_plane_list) - body_plane_list[::-1].index('2') - plane0 - plane1
+     #   slice_amount_per_plane = [plane0,plane1,plane2]
+        
+        # NumPy transformations
+        attribs = np.array(attribs_as_floats_lists, dtype=np.float64)
+    #    body_plane = np.array(body_plane_list, dtype=np.int64)
+    #    slice_numbers = np.array(slicenum_list, dtype=np.int64)
+    #    slice_amount = np.array(slice_amount_per_plane, dtype=np.int64)
+    
+    return  attribs
+
+
+def get_image_ID(attributes_filename):
+    all_image_id = re.findall(r'I[0-9]+',attributes_filename) # returns a array with all regular exp matches
+    if len(all_image_id) > 0:
+        return all_image_id[0]
+    else:
+        return ''
+
+def load_all_data_using_filters(attributes_dir, input_csv, valid_genders=['M','F'], min_age=0.0, max_age=200.0, debug=False, black_list_id=[]):
+    all_ref_attribs = []
+    all_ref_slices = []
+    #all_body_planes = []
+    #all_slice_num = []
+    #all_slice_amounts = []
+    all_classes = []
+    all_genders = []
+    all_ages = []
+    
+    
+    #image_id_dictionary  = build_cvs_dictionary(csv_file)
+    
+    
+    dic = build_csv_dictionary2(input_csv)
+    
+    #img_demo_dic = get_image_demographic_data_by_id2('I120779',dic)
+    # img_demo_dic=  {'class': 0, 'gender': 'M', 'age': 82, 'ref_slices': ['37', '', '']}
+    
+    # Getting all attributes files from attributes directory
+    attribs_files = list_dir.list_files(attributes_dir,".txt")
+    
+    print('black_list_id=',black_list_id)
+    
+    # Loop which loads attributes, demographics values and slicing info
+    for file in attribs_files:
+        image_id = get_image_ID(file)
+        try:
+            black_list_id.index(image_id)
+            print('attributes from image_id=\'{0}\' was not loaded because it is blacklisted!'.format(image_id))
+            #continue # if image_id was found in blacklist, skip current iteration
+        except ValueError:
+            
+            # image ID was not found in blacklist so we can extract ref_slices attributes from this volume...
+            if debug: print('\t* extracting data from file:',file)
+            demographics_data = dic[image_id]
+            if debug: print('\t* this file demographics: ',demographics_data)
+            gender = demographics_data['gender']
+            if debug: print('\t* gender loaded: ',gender)
+            age = demographics_data['age']
+            if debug: print('\t* age loaded: ', age)
+            
+            age_is_valid = age >= min_age and age <= max_age
+            if debug: print('\t* age_is_valid: ', age_is_valid)
+            
+            try:
+                if valid_genders.index(gender) >= 0:
+                    gender_is_valid = True
+            except ValueError:
+                gender_is_valid = False
+                
+            if debug: print('gender_is_valid: ',gender_is_valid)
+            
+            if gender_is_valid and age_is_valid:
+                if debug: print('Great! Both gender and age are valid!')
+                
+                
+                img_demo_dic = get_image_demographic_data_by_id2(image_id, dic)
+                ref_slices = img_demo_dic['ref_slices']
+                load_ref_slices_attribs(file, ref_slices)
+                
+                attribs = load_ref_slices_attribs(file, ref_slices)
+                
+                all_ref_attribs.append(attribs)
+                all_ref_slices.append(ref_slices)
+                
+                #all_slice_num.append(slice_num)
+                #all_slice_amounts.append(slice_amounts)  
+            
+                #demographics_data = get_image_demographic_data(file,image_id_dictionary)
+                
+                image_class = img_demo_dic['class']
+    
+                all_classes.append(image_class)
+                all_genders.append(gender)
+                all_ages.append(float(age))
+        
+    array_all_classes = np.array(all_classes, dtype=np.int64)
+    array_all_ages = np.array(all_ages, dtype=np.int64)
+    
+    print('*** {0:04d} attributes files were analysed and {1:04d} atributes were extracted'.format(len(attribs_files),len(all_genders)))
+    
+    return all_ref_attribs, array_all_classes, all_genders, array_all_ages, img_demo_dic
+
+    # getting partition from 80 to 100th slice from the f-th (f=0) attribs file
+    #partition = get_attributes_from_a_range_of_slices(attribs,slice_amounts,p,fs,ls)
+    #print('\n-Shape of Attributes Partition of the {0}th data file from the slices {2} and {3}: {1}'.format(f,partition.shape,fs,ls))
+    
+    #first = fs
+    #for at in partition:
+    #    print('\n-Attributes from {1}th slice of the {2}th attribs file:\n{0}'.format(at,first,f))
+    #    first = first + 1
+    
+    #print('\nPartition:\n{0}'.format(partition))
+
+
+# TEMPORARIA (para testes)
+def find_attributes_file(target_image_id, csv_dic, attributes_dir='../../attributes2'):
+    
+    #dic = build_csv_dictionary2(input_csv)
+    
+    #img_demo_dic = get_image_demographic_data_by_id2('I120779',dic)
+    # img_demo_dic=  {'class': 0, 'gender': 'M', 'age': 82, 'ref_slices': ['37', '', '']}
+    
+    # Getting all attributes files from attributes directory
+    attribs_files = list_dir.list_files(attributes_dir,".txt")
+    
+    for file in attribs_files:
+        image_id = get_image_ID(file)
+        
+        if image_id == target_image_id:
+            return file
+    
+    
 
 def main(argv):
     ## Testting area
@@ -191,9 +383,23 @@ def main(argv):
     
     
     #print(dic)
+    img_name = 'I120779'
+    img_demo_dic = get_image_demographic_data_by_id2(img_name,dic)
+    print('img_demo_dic= ', img_demo_dic)
+    ref_slices = img_demo_dic['ref_slices']
+    print('ref_slices from image ',img_name,'= ',ref_slices)
     
-    img_demo_dic = get_image_demographic_data_by_id2('I120779',dic)
-    print('ref_slices=',img_demo_dic)
+    attribs_file = find_attributes_file(img_name,dic)
+    
+    print('target attributes file = ', attribs_file)
+    
+    ref_attributes = load_ref_slices_attribs(attribs_file, ref_slices)
+    
+    print('ref_attributes from ref_slices {0} of image {1} are =\n{2}'.format(img_demo_dic['ref_slices'],img_name,ref_attributes))
+    
+    
+    
+    
     
     
     
